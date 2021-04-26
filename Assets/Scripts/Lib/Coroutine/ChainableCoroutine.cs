3ed4612713f10119435e7936b9d75488e7845f14
Christopher Mckerracher <ChrisLMckerracher@gmail.com>
2021-04-25 22:16:54 -0400

    Update ChainableCoroutine.cs
    Added comments in ChainableCoroutine explaining ToDos

--- Assets/Scripts/Lib/Coroutine/ChainableCoroutine.cs
+++ Assets/Scripts/Lib/Coroutine/ChainableCoroutine.cs
@@ -5,7 +5,8 @@ using System.Collections;
 public class ChainableCoroutine : MonoBehaviour {
 
     public IEnumerator chain(Func<Coroutine>[] requests) {
-
+        //This should not be a while loop, and was left in for testing ToDo: Remove this
+        // ToDo have some way to communicate that the chain is complete
         while(true) {
             foreach (Func<Coroutine> request in requests)
                 yield return request();
